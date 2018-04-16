d4f989bf9d935bd3cdced9f2a0caeb477dc7d62a
mackevin1 <kevin.portellas@gmail.com>
2018-04-16 23:46:56 +0200

    ver 1.91

--- aglans/aglans/urls.py
+++ aglans/aglans/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
-from products import views
+from boards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
