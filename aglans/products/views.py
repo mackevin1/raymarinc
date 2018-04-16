79bd16c1f2612c8472e29a73780a3d2e55bda3f0
mackevin1 <kevin.portellas@gmail.com>
2018-04-16 10:57:00 +0200

    ver 1.5

--- aglans/products/views.py
+++ aglans/products/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Board
 # Create your views here.
 def post_list(request):
     return render(request, 'products/index.html',{})
 def index(request):
      return render(request, 'index.html', {})
+def home(request):
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
