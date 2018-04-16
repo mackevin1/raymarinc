6d035eb45cf02042a1b23fcd8af7383770953657
mackevin1 <kevin.portellas@gmail.com>
2018-04-16 23:55:39 +0200

    ver 1.91

--- aglans/boards/models.py
+++ aglans/boards/models.py
@@ -13,7 +13,7 @@ class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE,)
-    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE,)
+    #starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE,)
 
 
 class Post(models.Model):
