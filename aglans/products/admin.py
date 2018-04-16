2be94159b2b469522cb67b4c97453e7949584c5a
mackevin1 <kevin.portellas@gmail.com>
2018-04-16 21:57:46 +0200

    ver 1.7

--- aglans/products/admin.py
+++ aglans/products/admin.py
@@ -10,23 +10,3 @@ from products.models import Board
 admin.site.register(Products)
 admin.site.register(Comment)
 admin.site.register(Board)
-
-from adminsortable2.admin import SortableAdminMixin
-from shop.admin.product import CMSPageAsCategoryMixin, ProductImageInline
-from .models import SmartCard
-
-
-@admin.register(SmartCard)
-class SmartCardAdmin(SortableAdminMixin, CMSPageAsCategoryMixin, admin.ModelAdmin):
-    fieldsets = (
-        (None, {
-            'fields': ('product_name', 'slug', 'product_code', 'unit_price', 'active', 'description',),
-        }),
-        (_("Properties"), {
-            'fields': ('manufacturer', 'storage', 'card_type', 'speed',)
-        }),
-    )
-    inlines = (ProductImageInline,)
-    prepopulated_fields = {'slug': ('product_name',)}
-    list_display = ('product_name', 'product_code', 'unit_price', 'active',)
-    search_fields = ('product_name',)
