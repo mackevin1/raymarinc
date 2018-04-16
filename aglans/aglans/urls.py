085a19011b60c1f31fa3f1feb7949766b07b900d
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 00:09:36 +0200

    ver 1.91

--- aglans/aglans/urls.py
+++ aglans/aglans/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from boards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
 ]
 
 urlpatterns += [
