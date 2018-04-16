ba603ec480b0e84b0e132417bb8ba4640d845fd0
mackevin1 <kevin.portellas@gmail.com>
2018-04-16 23:55:07 +0200

    ver 1.91

--- aglans/boards/models.py
+++ aglans/boards/models.py
@@ -21,5 +21,5 @@ class Post(models.Model):
     topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE,)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE,)
-    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE,)
+    #created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE,)
+    #updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE,)
