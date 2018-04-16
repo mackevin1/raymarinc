2c515b3040b9433e9ebecab90f8d4fe9cf494d3e
mackevin1 <kevin.portellas@gmail.com>
2018-04-16 23:25:06 +0200

    ver 1.8

--- aglans/products/tests.py
+++ aglans/products/tests.py
@@ -5,10 +5,10 @@ from .views import home
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
-        url = reverse('home')
+        url = reverse('index')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
-        self.assertEquals(view.func, home)
+        self.assertEquals(view.func, index)
