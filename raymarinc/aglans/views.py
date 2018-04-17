6b837a3e31f2a2ddbf20913c5a1dcc12d5517306
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 12:08:52 +0200

    ver 2.01

--- raymarinc/aglans/views.py
+++ raymarinc/aglans/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Board
+from .models import aglans
 
 def home(request):
     boards = Board.objects.all()
