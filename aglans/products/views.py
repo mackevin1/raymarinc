79c7383ca1e75aac99a67a4b383731ec1be1aa6a
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 09:12:13 +0200

    ver 1.92

--- aglans/products/views.py
+++ aglans/products/views.py
@@ -12,7 +12,7 @@ def post_list(request):
     return render(request, 'products/index.html',{})
 def index(request):
     boards = Products.objects.all()
-    return render(request, 'index.html', {'boards': boards})
+    return render(request, 'home.html', {'boards': boards})
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
