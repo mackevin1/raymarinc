87caf91c030faae96c7532a2c0557b649cd02d4a
mackevin1 <kevin.portellas@gmail.com>
2018-04-17 09:05:36 +0200

    ver 1.92

--- aglans/products/urls.py
+++ aglans/products/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-from boards import views as boards_views
 urlpatterns = [
 #    path('$/', views.products, name='products'),
 #    path('$/', views.comments, name='comments'),
     path('', views.index, name='index'),
-    path('$/', views.home, name='home'),
 ]
